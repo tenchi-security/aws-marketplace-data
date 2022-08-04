0ce7775e72309901bf6d41d391b093ae6e27bc5d
Alexandre Sieira <alexandre.sieira@gmail.com>
2022-08-04 16:35:32 -0300

    Add logging git pull in automation

--- aws-marketplace-data/main.py
+++ aws-marketplace-data/main.py
@@ -1,18 +1,30 @@
 import boto3
+import logging
+import sys
 from csv import writer
 from botocore.config import Config
 
-all_regions = [x['RegionName'] for x in boto3.client('ec2').describe_regions()['Regions']]
+logger = logging.getLogger("aws-marketplace_data")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+all_regions = sorted([x['RegionName'] for x in boto3.client('ec2').describe_regions()['Regions']])
 
 with open("amis.csv", 'w', newline='') as csvfile:
     csvwriter = writer(csvfile)
     csvwriter.writerow(
         ['Region', 'ImageId', 'CreationDate', 'Name', 'PlatformDetails', 'ProductCode', 'State', 'SnapshotIds',
          'OwnerId'])
+    entries = 0
     for region in all_regions:
+        logger.info(f"Processing {region}...")
         client = boto3.client("ec2", config=Config(region_name=region))
         for image in client.describe_images(Owners=["aws-marketplace"], IncludeDeprecated=True)['Images']:
-            for product_code in image.get('ProductCodes',[]):
+            for product_code in image.get('ProductCodes', []):
                 # combine valid snapshot IDs
                 snapshot_ids = [x['Ebs']['SnapshotId'] for x in image['BlockDeviceMappings']
                                 if 'Ebs' in x and 'SnapshotId' in x['Ebs']]
@@ -21,3 +33,7 @@ with open("amis.csv", 'w', newline='') as csvfile:
                 csvwriter.writerow([region, image['ImageId'], image['CreationDate'], image['Name'],
                                     image['PlatformDetails'], product_code['ProductCodeId'], image['State'],
                                     ','.join(snapshot_ids), image['OwnerId']])
+                entries += 1
+        logger.info(f"Ended {region}, {entries} written so far.")
+
+logger.info("Processing ended successfully.")
